620fa7c227298ab0fec176ff0a1aed49e82f39cc
GeoJokha <giorgi.joha@gmail.com>
2024-05-08 16:49:14 +0400

    Add project files.

---
+++ G19/DatabaseImporter.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+
+namespace G19_ProductImport
+{
+    public class DatabaseImporter
+    {
+        public void ImportData(IEnumerable<Category> categories)
+        {
+            string connectionString = "Server=DESKTOP-ANU2LNI; Database=Products Task; Integrated Security=true; TrustServerCertificate=true";
+            Database database = new Database(connectionString);
+            var command = database.OpenConnection().CreateCommand();
+
+            command.CommandText = "sp_ImportProduct";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@CategoryName", SqlDbType.NVarChar);
+            command.Parameters.Add("@CategoryIsActive", SqlDbType.Bit);
+            command.Parameters.Add("@ProductCode", SqlDbType.NVarChar);
+            command.Parameters.Add("@ProductName", SqlDbType.NVarChar);
+            command.Parameters.Add("@ProductPrice", SqlDbType.Decimal);
+            command.Parameters.Add("@ProductIsActive", SqlDbType.Bit);
+
+            foreach (var category in categories)
+            {
+                foreach (var product in category.Products)
+                {
+                    command.Parameters["@CategoryName"].Value = category.Name;
+                    command.Parameters["@CategoryIsActive"].Value = category.IsActive;
+                    command.Parameters["@ProductCode"].Value = product.Code;
+                    command.Parameters["@ProductName"].Value = product.Name;
+                    command.Parameters["@ProductPrice"].Value = product.Price;
+                    command.Parameters["@ProductIsActive"].Value = product.IsActive;
+                    command.ExecuteNonQuery();
+                }
+            }
+            database.CloseConnection();
+        }
+    }
+}
---
+++ G19/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace G19_ProductImport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string filePath = @"D:\products2.txt";
+            const string connectionString = "Server=DESKTOP-ANU2LNI; Database=Products Task; Integrated Security=true; TrustServerCertificate=true";
+            
+            Database database = new Database(connectionString);
+            DatabaseImporter importer = new DatabaseImporter();
+
+            importer.ImportData(FileManager.ReadData(filePath));       
+            
+            //database.OpenConnection();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                var categories = FileManager.ReadData(filePath);
+                foreach (var category in categories)
+                {
+                    Console.WriteLine(category);
+                    foreach(var product in category.Products)
+                    {
+                        Console.WriteLine($"\t{product}");
+                    }
+                }
+
+                //importer.ImportData(categories);
+                //var importer = new DatabaseImporter(connectionString);
+                //Console.WriteLine("Data imported.");
+                stopwatch.Stop();
+                Console.WriteLine($"{stopwatch.ElapsedMilliseconds} - Time");
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //finally
+            //{
+            //    database.CloseConnection();
+            //}            
+        }
+    }
+}
